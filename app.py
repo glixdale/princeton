5bf6f2bb42eedc6b35a1d0f591ce53d09c02a001
glixdale <ian.gill@live.com>
2023-12-07 15:25:56 -0500

    Update app.py
    properties db

--- app.py
+++ app.py
@@ -12,6 +12,25 @@ class Todo(db.Model):
     
     def __repr__(self):
         return '<Task %r>' % self.id
+    
+
+class Properties(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    Street_Number = db.Column(db.Integer, nullable=False)
+    Street_Name = db.Column(db.String(200), nullable=False)
+    Address_Line_2 = db.Column(db.String(200), nullable=False)
+    City = db.Column(db.String(200), nullable=False)
+    Zip = db.Column(db.Integer, nullable=False)
+    State = db.Column(db.String(50), nullable=False)
+    Phone = db.Column(db.String(10), nullable=False)
+    Phone2 = db.Column(db.String(10), nullable=True)
+    Phone3 = db.Column(db.String(10), nullable=True)
+    Fax = db.Column(db.String(10), nullable=True)
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
 
 
 @app.route("/", methods=['POST', 'GET'])
